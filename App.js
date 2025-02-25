617cf3ce0ce7ecae8ae530d4d7134ec6567dc9ef
timok81 <timokujansuu@gmail.com>
2025-02-25 11:38:37 +0100

    Implemented asyncStorage

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+  getFirestore,
+  disableNetwork,
+  enableNetwork,
+} from "firebase/firestore";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 import Start from "./components/Start.js";
 import Chat from "./components/Chat.js";
@@ -9,6 +16,18 @@ import Chat from "./components/Chat.js";
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  const connectionStatus = useNetInfo();
+
+  //useNetInfo checks for internet connection, this is used for determining whether to display database data or local data
+  useEffect(() => {
+    if (connectionStatus.isConnected === false) {
+      Alert.alert("Connection Lost!");
+      disableNetwork(db);
+    } else if (connectionStatus.isConnected === true) {
+      enableNetwork(db);
+    }
+  }, [connectionStatus.isConnected]);
+
   const firebaseConfig = {
     apiKey: "AIzaSyBFR7FMIJeSarJUkQLT7MM34AmQdyQQmpk",
     authDomain: "cf-chat-app-ec088.firebaseapp.com",
@@ -26,7 +45,13 @@ const App = () => {
       <Stack.Navigator initialRouteName="Start">
         <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="Chat">
-          {(props) => <Chat db={db} {...props} />}
+          {(props) => (
+            <Chat
+              isConnected={connectionStatus.isConnected}
+              db={db}
+              {...props}
+            />
+          )}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import { useEffect, useState } from "react";
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import {
   collection,
   addDoc,
@@ -9,31 +9,59 @@ import {
   orderBy,
 } from "firebase/firestore";
 import PropTypes from "prop-types";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
   const { name, bgColor, userID } = route.params;
   const [messages, setMessages] = useState([]);
 
+  //This must be declared outside of useEffect so that the old listener gets removed
+  let unsubChat;
+
   useEffect(() => {
     navigation.setOptions({ title: name });
-    const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-    
-    //Listen for database events
-    const unsubChat = onSnapshot(q, (documents) => {
-      let newMessages = [];
-      documents.forEach((document) => {
-        newMessages.push({
-          id: document.id,
-          ...document.data(),
-          createdAt: new Date(document.data().createdAt.toMillis()),
+
+    //If connected to internet, use db. If not, use AsyncStorage
+    if (isConnected === true) {
+      if (unsubChat) unsubChat();
+      unsubChat = null;
+      const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+
+      //Listen for database events to update messages in realtime
+      unsubChat = onSnapshot(q, (documents) => {
+        let newMessages = [];
+        documents.forEach((document) => {
+          newMessages.push({
+            id: document.id,
+            ...document.data(),
+            createdAt: new Date(document.data().createdAt.toMillis()),
+          });
         });
+        cacheMessages(newMessages);
+        setMessages(newMessages);
       });
-      setMessages(newMessages);
-    });
+    } else loadCachedMessages();
+
+    //Cleanup
     return () => {
       if (unsubChat) unsubChat();
     };
-  }, []);
+  }, [isConnected]);
+
+  //use AsyncStorage to store messages locally. It can only store strings, so it must be stringified.
+  const cacheMessages = async (messagesToCache) => {
+    try {
+      await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  //use AsyncStorage to load cached emssages
+  const loadCachedMessages = async () => {
+    const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+    setMessages(JSON.parse(cachedMessages));
+  };
 
   //Send message handler
   const onSend = (newMessages) => {
@@ -57,12 +85,20 @@ const Chat = ({ route, navigation, db }) => {
     );
   };
 
+  //Display input field only if online
+  const renderInputToolbar = (props) => {
+    if (isConnected) {
+      return <InputToolbar {...props} />;
+    } else return null;
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: bgColor || "#474056" }]}>
       {/* Renders chat */}
       <GiftedChat
         messages={messages}
         renderBubble={renderBubble}
+        renderInputToolbar={renderInputToolbar}
         onSend={(messages) => onSend(messages)}
         user={{
           _id: userID,
